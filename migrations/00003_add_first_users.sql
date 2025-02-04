f8747ad7d45bae3baa0d0d1c708cf98a8c565dfa
Paul <159115191+paulparfe@users.noreply.github.com>
2025-02-04 19:07:51 +0300

    Remove hardcoded ids from migration

--- migrations/00003_add_first_users.sql
+++ migrations/00003_add_first_users.sql
@@ -1,12 +1,12 @@
 -- +goose Up
 -- +goose StatementBegin
-INSERT INTO users (id, name, balance) VALUES (1, 'Adam', 111.11);
-INSERT INTO users (id, name, balance) VALUES (2, 'Diana', 12.34);
+INSERT INTO users (name, balance) VALUES ('Adam', 111.11);
+INSERT INTO users (name, balance) VALUES ('Diana', 12.34);
 
-INSERT INTO transactions (id, user_id, recipient_id, amount, transaction_type, created_at)
-    VALUES (1, 1, NULL, 123.45, 'deposit', '2025-02-03 11:11:11');
-INSERT INTO transactions (id, user_id, recipient_id, amount, transaction_type, created_at)
-    VALUES (2, 1, 2, 12.34, 'transfer', '2025-02-04 12:12:12');
+INSERT INTO transactions (user_id, recipient_id, amount, transaction_type, created_at)
+    VALUES (1, NULL, 123.45, 'deposit', '2025-02-03 11:11:11');
+INSERT INTO transactions (user_id, recipient_id, amount, transaction_type, created_at)
+    VALUES (1, 2, 12.34, 'transfer', '2025-02-04 12:12:12');
 
 -- +goose StatementEnd
 
